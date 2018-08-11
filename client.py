2a9f1688cfdc9e9b266a1efb3445ed87e45bc1a7
Pierre Gergis <pierre.s.gergis@gmail.com>
2018-08-11 14:47:59 -0400

    Refactor, plus ip parse fix

--- client.py
+++ client.py
@@ -6,67 +6,66 @@ import requests
 import sys
 import time
 
+## Unique machine ID: peer_id
+pid = os.getpid()
+curtime = time.time()
+peer_id = hashlib.sha1(bytes(str(int(curtime*pid)).encode())).digest()
+print(peer_id)
+
+class Torrent(object):
+    def __init__(self, filename):
+        self.torrent_hash = be.filename_decode(filename)
+        self.info_hash = hashlib.sha1(be.encode(self.torrent_hash[b'info'])).digest()
+
+        self.total_bytes = 0
+        try:
+            for f in self.torrent_hash[b'info'][b'files']:
+                self.total_bytes += f[b'length']
+        except KeyError:
+            self.total_bytes += self.torrent_hash[b'info'][b'length']
+
+
 def compact_peers_decode(peer_bytes):
     peers = {}
     with io.BytesIO(peer_bytes) as b:
         peer = b.read(6)
         while len(peer) == 6:
-            ip = int.from_bytes(peer[:4], 'big')
+            ip = '.'.join([str(int(n)) for n in peer[:4]])
             port = int.from_bytes(peer[4:6], 'big')
             peers[ip] = port
             peer = b.read(6)
     if len(peer) != 0:
         raise Exception(f'{len(peer)} residual peer bytes')
-    print(peers)
+    return peers
 
-def main():
-
-    filename = 'testfiles/archlinux-2018.08.01-x86_64.iso.torrent'
-    torrent = be.filename_decode(filename)
-
-    # print(torrent[b'info'])
-    info_hash = hashlib.sha1(be.encode(torrent[b'info'])).digest()
-    print(info_hash)
-
-    curtime = time.time()
-    pid = os.getpid()
-    myid = int(curtime * pid)
-    myid = bytes(str(myid).encode())
-    peer_id = hashlib.sha1(myid).digest()
-    # print(peer_id)
-
-    port = 6885
-
-    ul_bytes = 0
-
-    dl_bytes = 0
-
-    total_bytes = 0
-    try:
-        for f in torrent[b'info'][b'files']:
-            total_bytes += f[b'length']
-    except KeyError:
-        total_bytes += torrent[b'info'][b'length']
-
-    event = 'started'
-
+def send_tracker_request(torrent, ul_bytes, dl_bytes, event='', port=6885, compact=1, no_peer_id=''):
     request_params = {
-        'info_hash': info_hash, # 20-byte SHA1 hash of torrent[info]
+        'info_hash': torrent.info_hash, # 20-byte SHA1 hash of torrent[info]
         'peer_id': peer_id, # 20 byte SHA1 hash of machineID (timestamp+pid?)
         'port': port, # 6881-6889 (usually); give up if can't establish port in range
         'uploaded': ul_bytes, # amount uploaded since start in base ten ASCII (total bytes)
         'downloaded': dl_bytes, # amount downloaded since start in base ten ASCII (total bytes)
-        'left': total_bytes - dl_bytes, # amount left to download, base ten ASCII (total bytes remaining)
+        'left': torrent.total_bytes - dl_bytes, # amount left to download, base ten ASCII (total bytes remaining)
         'compact': 1, # 1 to accept: peers_list -> peers_string w/ 6 bytes per peer -- 4:host+2:port (network byte order)
         'no_peer_id': '', # ignored w/ compact mode
         'event': event # must be 'started', 'stopped', 'completed', or ''
     }
 
-    announce = torrent[b'announce'].decode()
-    print(announce)
+    announce = torrent.torrent_hash[b'announce'].decode()
     response = requests.get(announce, params=request_params)
     response = be.string_decode(response.content)
-    compact_peers_decode(response[b'peers'])
+    peers = compact_peers_decode(response[b'peers'])
+    return peers
+
+def main():
+
+    filename = 'testfiles/archlinux-2018.08.01-x86_64.iso.torrent'
+    torrent = Torrent(filename)
+    event = 'started'
+    uploaded_bytes = 0
+    downloaded_bytes = 0
+    print(send_tracker_request(torrent, uploaded_bytes, downloaded_bytes, event))
+
 
 
 if __name__ == '__main__':
